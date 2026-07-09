bbd8d440c430ccc5bb018cc2a91c2591566591a0
Diego <dieguinoxjair@gmail.com>
2026-07-08 22:23:41 -0600

    frontend

--- TiendaAPI/obj/Debug/net8.0/TiendaAPI.AssemblyInfo.cs
+++ TiendaAPI/obj/Debug/net8.0/TiendaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9fde4d9894af9761c230c54a187228f827eff5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
